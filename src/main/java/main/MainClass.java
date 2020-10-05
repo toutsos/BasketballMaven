6ad977de7a8894633d7aa97f67e0f770933bf90f
Angelos Toutsios <Ca.a.toutsios@army.gr>
2020-10-05 12:29:58 +0300

    created DBUtil.class

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -13,7 +13,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-public class main {
+public class MainClass {
 
     public static void main(String[] args) {
         CoachDao cd = new CoachDao();
